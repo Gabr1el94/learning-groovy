375a567dbc960d9f8d431b7eca80c4798596d87b
Gabr1el94 <bielsystem64@gmail.com>
2023-09-27 12:26:46 -0300

    understand variables and data types

---
+++ examples/dataTypes.groovy
@@ -0,0 +1,61 @@
+/** Data Types
+ byte|short|int|long|float|double|char|Boolean|String
+**/
+
+//byte
+println "Byte:"
+println Byte.MIN_VALUE
+println Byte.MAX_VALUE
+
+println '--------------'
+
+println "Short:"
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+
+println '--------------'
+
+println "Integer:"
+println Integer.MIN_VALUE
+println Integer.MAX_VALUE
+
+println '--------------'
+
+println "Short:"
+println Short.MIN_VALUE
+println Short.MAX_VALUE
+
+println '--------------'
+
+println "Long:"
+println Long.MIN_VALUE
+println Long.MAX_VALUE
+
+println '--------------'
+
+println "Float:"
+println Float.MIN_VALUE
+println Float.MAX_VALUE
+
+println '--------------'
+
+println "Double:"
+println Double.MIN_VALUE
+println Double.MAX_VALUE
+
+println '--------------'
+
+println "Char:"
+char ch = 'A'
+println "caracter ${ch}"
+
+println '--------------'
+println "String:"
+String str = "Groovy"
+println " exemplo ${str}"
+
+println '--------------'
+
+println "Boolean:"
+boolean flag = true
+println flag
---
+++ examples/variables.groovy
@@ -0,0 +1,11 @@
+/*Variables*/ 
+// letters, digits, underscore(_)
+// case sensitive
+// dynamically typed
+def name = "Teste"
+println 'Name is ${name}' //apóstofre
+println "Name is ${name}" //aspas
+
+def (String a, int b) = ["Gabriel", 28]
+
+println "Meu nome é ${$a} e tenho ${b} anos de idade."
